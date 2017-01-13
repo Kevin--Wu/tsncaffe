d6ab0cf62d19dff91e9e3e76db6e794c4dfc4c75
Kevin_win7 <wuhaoxuan1993@163.com>
2017-01-13 15:40:25 +0800

    modify

--- mywork/ucf101/predict.py
+++ mywork/ucf101/predict.py
@@ -60,6 +60,83 @@ def rgb_predict():
 #	rgbpre.write('%d %d\n' % (acnum,totalnum))
 	rgbpre.close()
 	rgblabel.close()
-	return acnum,totalnum	
+	return acnum,totalnum
 
-print rgb_predict()
+def flow_predict() #光流输入的格式是xyxyxy...
+	data_root='/home/hadoop/whx/dataset/ucf101/ucf101_flow_img_tvl1_gpu/'
+	net = caffe.Net(caffe_root + 'mywork/ucf101/tsn_bn_inception_flow_deploy.prototxt',model_root +'model/tsp-bn-ucf1-flow-withpre/ucf101_split1_tsn_flow_bn_inception_iter_80000.caffemodel',caffe.TEST)
+	flowpre=open(caffe_root+'mywork/ucf101/flowpredict.txt','w')
+	flowlabel=open(caffe_root+'mywork/ucf101/flowlabel.txt','w')
+
+
+	transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+	transformer.set_transpose('data', (2,0,1))
+	transformer.set_raw_scale('data', 255)  
+
+	actdirs=os.listdir(data_root)
+	actdirs.sort()
+	actid=0
+	acnum=0
+	totalnum=0
+	for curact in actdirs:
+		if os.path.isdir(data_root+curact):
+			videodirs=os.listdir(data_root+curact)
+			videodirs.sort()
+			for curvideo in videodirs:
+				if os.path.isdir(data_root+curact+'/'+curvideo):
+					framelist=os.listdir(data_root+curact+'/'+curvideo)
+					framelist.sort()
+					framenum = len(framelist)
+					i=1
+					count=0
+					segnum=3
+					flow_length=5
+					stopid=(framenum/3)-flowlength*2+1
+					totalout=[0]*101
+					
+					while i<stopid:
+						curseg=0
+						inputdata=[]
+						while curseg<segnum:
+							frameid = i + curseg*(framenum/3)
+							j=0
+							imagexpath=data_root+curact+'/'+curvideo+'/'+('flow_x_{:0>4d}.jpg'.format(frameid+j))
+							imageypath=data_root+curact+'/'+curvideo+'/'+('flow_y_{:0>4d}.jpg'.format(frameid+j))
+							imagex=caffe.io.load_image(imagexpath)
+							imagey=caffe.io.load_image(imageypath)
+							imagesg=concatenate((transformer.preprocess('data',imagex),transformer.preprocess('data',imagey)),axis=0)
+							j+=1
+							while j<flow_length:
+								imagexpath=data_root+curact+'/'+curvideo+'/'+('flow_x_{:0>4d}.jpg'.format(frameid+j))
+								imageypath=data_root+curact+'/'+curvideo+'/'+('flow_y_{:0>4d}.jpg'.format(frameid+j))
+								imagex=caffe.io.load_image(imagexpath)
+								imagey=caffe.io.load_image(imageypath)
+								imagesg=concatenate((imagesg,concatenate((transformer.preprocess('data',imagex),transformer.preprocess('data',imagey)),axis=0)),axis=0)
+								j+=1
+
+							inputdata.append(imagesg)
+
+						net.blobs['data'].reshape(3,10,224,224)
+						net.blobs['data'].data[...] = inputdata
+						net.forward()
+
+						out = net.blobs['pool_fc'].data[...]
+						i+=2
+						count+=1
+						totalout=totalout+(out[0][0][0]-totalout)/count
+
+					
+					
+					print >> flowpre, totalout
+					prob=out.argmax()
+					flowlabel.write('%d %d\n' % (prob,actid))
+					if prob == actid:
+						acnum+=1
+					totalnum+=1
+		actid+=1
+#	rgbpre.write('%d %d\n' % (acnum,totalnum))
+	flowpre.close()
+	flowlabel.close()
+	return acnum,totalnum
+
+print flow_predict()
