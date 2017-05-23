d32d8a191d761b7667ff45679252e53ae7a91a2c
kevin--wu--gpu1 <wuhaoxuan1993@163.com>
2017-05-23 17:04:02 +0800

    modify

--- mywork/ucf101/piFramePredict.py
+++ mywork/ucf101/piFramePredict.py
@@ -79,7 +79,7 @@ def rgb_frame_predict():
 
 def flow_frame_predict():	#The format of flow imgs is flowx flowy flowx flowy
 	data_root='/home/hadoop/whx/dataset/ucf101/ucf101_flow_img_tvl1_gpu/'
-	net = caffe.Net(caffe_root + 'mywork/ucf101/pi_tsn_bn_inception_flow_deploy_frame.prototxt',model_root +'model/pi-bn-ucf1-flow-withpre/pi_ucf101_split1_tsn_flow_bn_inception_iter_80000.caffemodel',caffe.TEST)
+	net = caffe.Net(caffe_root + 'mywork/ucf101/pi_tsn_bn_inception_flow_deploy_frame.prototxt',model_root +'model/pi-bn-ucf1-flow/pi_ucf101_split1_tsn_flow_bn_inception_iter_80000.caffemodel',caffe.TEST)
 	flowpre=open(caffe_root+'mywork/ucf101/flowpredict.txt','w')
 	flowlabel=open(caffe_root+'mywork/ucf101/flowlabel.txt','w')
 
@@ -150,4 +150,4 @@ def flow_frame_predict():	#The format of flow imgs is flowx flowy flowx flowy
 	flowlabel.close()
 	return acnum,totalnum
 
-print rgb_frame_predict()
+print flow_frame_predict()
